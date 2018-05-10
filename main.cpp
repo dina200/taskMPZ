fa1d1c3a04bf6e0698d2b7d7bf4813c9a0fe65af
dina200 <dinapeace@ya.ru>
2018-05-10 19:05:24 +0300

    Third commit

--- main.cpp
+++ main.cpp
@@ -3,19 +3,22 @@
 
 int main() {
 	pstorage ob, ts, sts, svs;
-	initlist(50); //* 5 * 5);
+	initlist(50);
+	pqueue qob;
 	newstorage(ob, "Otlivochnie bloki", 14);
 	newstorage(ts, "Tokarnie stanki", 5);
 	newstorage(sts, "Strogal'nie stanki", 4);
 	newstorage(svs, "Sverliynie stanki", 8);
+	newqueue(qob, "Ochered' dlya otlivochnih blokov");
 	initcreate(1,0);
-	initcreate(11,0);
-	starttrace();	
-	while (systime < 8 * 60) { //5 * 40
+	starttrace();
+	while (systime < 8 * 60) { //
 		plan();
 		switch(sysevent) {
-			case 1: create(8*60/50);
-			case 2: if(randab(0,1,v1) > 0.24) {
+			case 1: create(8*60/50); break;
+			case 2: inqueue(qob); break;
+			case 3: if (qob->lq > 14) wait(5); break;
+			case 4: if(randab(0,1,v1) > 0.24) {
 					trans->pb[0] = true;
 					enter(ob, 1); 					
 					break;
@@ -24,34 +27,36 @@ int main() {
 					trans->pb[0] = false;	
 					enter(svs, 1);			
 					break;
-				}			
-			case 3: if (trans->pb[0]) delayt (125); 
+				}
+				
+			case 5: outqueue(qob); break;			
+			case 6: if (trans->pb[0]) delayt (125); 
 					else  delayt (105);
 					break;					
-			case 4: if (trans->pb[0]) leave(ob, 1); 
+			case 7: if (trans->pb[0]) leave(ob, 1); 
 					else  leave(svs, 1);
 					break; 
 			
-			case 5: if (trans->pb[0]) enter (sts, 1); 
+			case 8: if (trans->pb[0]) enter (sts, 1); 
 					else  enter (ts, 1);
 					break;					
-			case 6: if (trans->pb[0]) delayt (35); 
+			case 9: if (trans->pb[0]) delayt (35); 
 					else  delayt (90);
 					break;					
-			case 7: if (trans->pb[0]) leave(sts, 1); 
+			case 10: if (trans->pb[0]) leave(sts, 1); 
 					else  leave(ts, 1);
 					break;
 					
-			case 8: if (trans->pb[0]) enter (ts, 1); 
+			case 11: if (trans->pb[0]) enter (ts, 1); 
 					else  enter (sts, 1);
 					break;					
-			case 9: if (trans->pb[0]) delayt (20); 
+			case 12: if (trans->pb[0]) delayt (20); 
 					else  delayt (65);
 					break;					
-			case 10: if (trans->pb[0]) leave(ts, 1); 
+			case 13: if (trans->pb[0]) leave(ts, 1); 
 					else  leave(sts, 1);
 					break;
-			case 11:destroy(); break;
+			case 14:destroy(); break;
 		}
 	
 	}
